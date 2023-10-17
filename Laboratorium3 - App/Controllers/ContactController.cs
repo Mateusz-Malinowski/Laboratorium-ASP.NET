cf25d2c25b0944b4f59f9b637c9202cc1ad8e921
Mateusz Malinowski <mateusz.malinowski.343@gmail.com>
2023-10-17 16:41:49 +0200

    Add CRUD for Contact model

---
+++ Laboratorium3 - App/Controllers/ContactController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Laboratorium3___App.Models;
+
+namespace Laboratorium3___App.Controllers
+{
+    public class ContactController : Controller
+    {
+        private static readonly Dictionary<int, Contact> contacts = new Dictionary<int, Contact>();
+        private static int id = 1;
+
+        public IActionResult Index()
+        {
+            return View(contacts);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact model)
+        {
+            if (ModelState.IsValid) // validation of "Contact model"
+            {
+                model.Id = id++;
+                contacts.Add(model.Id, model);
+                return RedirectToAction("Index");
+            }
+
+            return View(model); // show form again - with errors
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(contacts[id]);    
+        }
+
+        [HttpPost]
+        public IActionResult Update(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                contacts[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View(contacts[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Contact model)
+        {
+            contacts.Remove(model.Id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(contacts[id]);
+        }
+    }
+}
---
+++ Laboratorium3 - App/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorium3___App.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "You must specify the name!")]
+        [StringLength(maximumLength: 50, ErrorMessage = "Too long name, max 50 chars")]
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string? Phone { get; set; }
+        public DateTime? Birth { get; set; }
+    }
+}
