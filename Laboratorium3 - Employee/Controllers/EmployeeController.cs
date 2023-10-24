7e07bb10fe78ca7cd93181408faf1d0861f53f93
Mateusz Malinowski <mateusz.malinowski@screen9.com>
2023-10-24 14:56:28 +0200

    Add Employee CRUD

---
+++ Laboratorium3 - Employee/Controllers/EmployeeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Laboratorium3___Employee.Models;
+
+namespace Laboratorium3___Employee.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private static readonly Dictionary<int, Employee> employees = new Dictionary<int, Employee>();
+        private static int id = 1;
+
+        public IActionResult Index()
+        {
+            return View(employees);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Employee());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee model)
+        {
+            if (ModelState.IsValid) // validation of "Employee model"
+            {
+                model.Id = id++;
+                employees.Add(model.Id, model);
+                return RedirectToAction("Index");
+            }
+
+            return View(model); // show form again - with errors
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(employees[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                employees[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View(employees[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Employee model)
+        {
+            employees.Remove(model.Id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(employees[id]);
+        }
+    }
+}
